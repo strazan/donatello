57cbd626df2cd7eaf801b3a52ff9a4bc01561913
strazan <sigge@labor.cool>
2019-10-25 18:06:54 +0200

    moving files first draft working, pref init

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ const path = require('path')
 
 const {
     app,
-    BrowserWindow
+    BrowserWindow,
+    Menu
 } = electron;
 
 let mainWindow;
+
+let orgDowPrefWindow;
 // will print something similar to /Users/maf/.../Electron
 
 
@@ -15,13 +18,61 @@ app.on('ready', function () {
     mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+         width: 900,
+        height: 600,
     });
 
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'main-window.html'),
+        pathname: path.join(__dirname, 'preferences.html'),
+    //    pathname: path.join(__dirname, 'main-window.html'),
         protocol: 'file:',
         slashes: true
-
     }))
+
+    // const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+
+    // Menu.setApplicationMenu(mainMenu)
+    // console.log(mainMenu)
+    // openPrefWindow();
 });
+
+const mainMenuTemplate = [{
+    label: 'File',
+    submenu: [{
+        label: 'Preferences',
+        click(){
+            openPrefWindow()
+        }
+    },
+    {
+        label: 'Quit',
+        click(){
+            app.quit();
+        }
+    }]
+}];
+
+function openPrefWindow() {
+    orgDowPrefWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+
+    });
+
+    orgDowPrefWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'organize-download-pref.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+  
+}
+
+//+ document.getElementById('scriptOrgDow'))
+// document.getElementById('scriptOrgDow').addEventListener('click', () => {
+
+//     orgDowPrefWindow.show = true;
+// })
--- scripts/app.js
+++ scripts/app.js
@@ -3,38 +3,110 @@ const fs = require('fs')
 const electron = require('electron')
 const {
     app,
-    BrowserWindow
+    BrowserWindow,
+    dialog
 } = electron;
 
-let isFileMoverRunning = false;
+let isFileMoverRunning = true;
 let toggleOne = document.getElementById('script-one');
+let baseFolder = `/Users/siggelabor/Desktop/folder-to/`;
 
-toggleOne.addEventListener('click', function () {
-    if (isFileMoverRunning) {
-        toggleOne.style.backgroundColor = 'salmon ';
-    } else {
-        toggleOne.style.backgroundColor = 'LightGreen ';
+let labelBasePath = document.getElementById('orgDownBasePath');
+labelBasePath.innerHTML = (baseFolder);
+let labelBasePathInput = document.getElementById('orgDownBasePathInput')
+// toggleOne.addEventListener('click', function () {
+//     isFileMoverRunning = !isFileMoverRunning
+//     if (isFileMoverRunning) {
+//         toggleOne.style.backgroundColor = '#fff';
+//     } else {
+//         toggleOne.style.backgroundColor = '#fff3ee';
+//     }
+
+// });
+
+labelBasePath.addEventListener('click', () => {
+
+
+    // dialog.showOpenDialog(mainWindow, {
+    //     properties: ['openDirectory']
+    // })
+
+
+    labelBasePathInput.click()
+    labelBasePathInput.onchange = function (e) {
+        labelBasePath.innerHTML = labelBasePathInput.value
+
     }
-    isFileMoverRunning = !isFileMoverRunning
 
-});
+})
 
-fs.watch('/Users/siggelabor/Desktop/folder-to/', (eventType, filename) => {
+
+
+
+
+
+fs.watch('/Users/siggelabor/Desktop/folder-to/', (eventType, file) => {
     if (isFileMoverRunning) {
         if (eventType === 'rename') {
-            let category = getCategory(filename);
+            let filename = file;
+            let category = getCategory(file);
+            let moveFrom = `${baseFolder}${filename}`
+            let moveTo = `${baseFolder}${category}/${filename}`
 
-            // let user change path
-            let moveFrom = `/Users/siggelabor/Desktop/folder-to/${filename}`
-            let moveTo = `/Users/siggelabor/Desktop/folder-to/${category}/${filename}`
-            fs.rename(moveFrom, moveTo, function (err) {
-                if (err) {
-                    throw err;
-                }
-            });
+            moveFile(moveFrom, moveTo, filename, category);
         }
     }
 });
+
+function moveFile(from, to, filename, cat) {
+
+    doesFileExist(to, exists => {
+        if (exists) {
+            newFileName(from, to, filename, cat);
+        } else {
+            renameFile(from, to)
+        }
+    })
+}
+
+function newFileName(moveFrom, moveTo, filename, cat) {
+
+    let newName = `${filename.split('.')[0]}-${getRandomID()}.${filename.split('.')[1]}`
+
+    doesFileExist(path + '/' + newName, callback => {
+        if (!callback) {
+            moveTo = `/Users/siggelabor/Desktop/folder-to/${cat}/${newName}`
+            renameFile(moveFrom, moveTo)
+        }
+    })
+}
+
+function renameFile(moveFrom, moveTo) {
+    fs.rename(moveFrom, moveTo, function (err) {
+        if (err) {
+            throw err;
+        }
+    });
+}
+
+function doesFileExist(moveTo, callback) {
+    fs.exists(moveTo, (exi) => {
+        if (exi) {
+            callback(true)
+        } else {
+            callback(false)
+        }
+    });
+}
+
+function getRandomID(len = 3, chars = 'abcdefghjkmnopqrstwxyz0123456789') {
+    let id = '';
+    while (len--) {
+        id += chars[Math.random() * chars.length | 0];
+    }
+    return id;
+}
+
 
 function getCategory(filename) {
     let arr = filename.split('.');
