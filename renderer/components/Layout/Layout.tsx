ec2859db74c4a0a9a79628eb33f09b7d7cce8e3c
sigge <siggelabor@hotmail.com>
2022-11-27 16:34:45 +0100

    🖼️ Layout

---
+++ renderer/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import MainFrame from 'components/MainFrame'
+import SideBar from 'components/SideBar'
+import React, { useState } from 'react'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    const [sideOpen, setSidebarOpen] = useState(false)
+    return (
+        <div className='flex min-h-screen relative h-full'>
+            <SideBar open={sideOpen} setOpen={setSidebarOpen} />
+            <MainFrame setSidebarOpen={setSidebarOpen}>
+                {children}
+            </MainFrame>
+        </div>
+    )
+}
