3f5b9c882e04ddf58df1dc5bfabbee7b7a712467
sigge <siggelabor@hotmail.com>
2022-12-01 22:51:11 +0100

    📦 Toggle

--- renderer/components/Toggle/Toggle.tsx
+++ renderer/components/Toggle/Toggle.tsx
@@ -14,14 +14,15 @@ export default function Toggle({ onChange, value, label }: ToggleProps) {
         <Switch
             checked={value}
             onChange={onChange}
-            className="group relative inline-flex h-5 w-10 flex-shrink-0 cursor-pointer items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            className="group relative inline-flex h-5 w-10 flex-shrink-0 cursor-pointer items-center justify-center 
+            rounded-full"
         >
             <span className="sr-only">{label}</span>
-            <span aria-hidden="true" className="pointer-events-none absolute h-full w-full rounded-md bg-white" />
+            <span aria-hidden="true" className="pointer-events-none absolute h-full w-full rounded-md bg-surface" />
             <span
                 aria-hidden="true"
                 className={classNames(
-                    value ? 'bg-indigo-600' : 'bg-gray-200',
+                    value ? 'bg-primary' : 'bg-containerActive',
                     'pointer-events-none absolute mx-auto h-4 w-9 rounded-full transition-colors duration-200 ease-in-out'
                 )}
             />
@@ -29,7 +30,7 @@ export default function Toggle({ onChange, value, label }: ToggleProps) {
                 aria-hidden="true"
                 className={classNames(
                     value ? 'translate-x-5' : 'translate-x-0',
-                    'pointer-events-none absolute left-0 inline-block h-5 w-5 transform rounded-full border border-gray-200 bg-white shadow ring-0 transition-transform duration-200 ease-in-out'
+                    'pointer-events-none absolute left-0 inline-block h-5 w-5 transform rounded-full border border-outline2 bg-surface shadow ring-0 transition-transform duration-200 ease-in-out'
                 )}
             />
         </Switch>
